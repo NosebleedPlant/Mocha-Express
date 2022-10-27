f7e8a2cabdd163fdff32a447095a5c5e88a03c8d
JTContreras95 <73808527+JTContreras95@users.noreply.github.com>
2022-10-27 15:49:37 -0300

    soundbank Conversion to Vorbis

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,47 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MUSIC_MUTE = 1696404602U;
+        static const AkUniqueID MUSIC_UNMUTE = 2132259803U;
         static const AkUniqueID PLAY_BOLT = 1340741931U;
+        static const AkUniqueID PLAY_COOKING = 4057174442U;
+        static const AkUniqueID PLAY_FOODREADY = 3707288775U;
+        static const AkUniqueID PLAY_ITEMPICKUP = 4241593335U;
+        static const AkUniqueID PLAY_ITEMPLACE = 1507987364U;
+        static const AkUniqueID PLAY_MENUCLICK = 2576276897U;
+        static const AkUniqueID PLAY_MENUSELECT = 40516875U;
+        static const AkUniqueID PLAY_SHOOT = 2641650631U;
+        static const AkUniqueID SFX_MUTE = 2835551186U;
+        static const AkUniqueID SFX_UNMUTE = 1305045635U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace COOKING
+        {
+            static const AkUniqueID GROUP = 2449017287U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOIL = 1660835247U;
+                static const AkUniqueID FRY = 898583890U;
+                static const AkUniqueID MIX = 1182670517U;
+            } // namespace SWITCH
+        } // namespace COOKING
+
+        namespace PLATEAMMO
+        {
+            static const AkUniqueID GROUP = 1081040201U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CANNED = 2292682242U;
+                static const AkUniqueID PLANETX = 2962930641U;
+                static const AkUniqueID VENUSIAN = 2469960200U;
+            } // namespace SWITCH
+        } // namespace PLATEAMMO
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
@@ -25,6 +64,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
