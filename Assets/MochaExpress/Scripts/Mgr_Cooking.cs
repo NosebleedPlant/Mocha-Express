6c6dded321430b3d92935f4f4a530c94dcbf9896
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2022-10-28 18:22:31 -0600

    added sound note

--- Assets/MochaExpress/Scripts/Mgr_Cooking.cs
+++ Assets/MochaExpress/Scripts/Mgr_Cooking.cs
@@ -154,7 +154,7 @@ public class Mgr_Cooking : MonoBehaviour
         spawnpos.z = _heldIngredient.position.z;
 
         CleanCookingArea();
-
+        //JTC Food Completeion sound here!
         return Instantiate<Collider2D>(mealPrefab[recipie],spawnpos,Quaternion.identity);
     }
 
